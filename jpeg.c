74ec00ae7bcf211cfc1e5033fed71953efda1309
Breno Nogueira <breno.s.nogueira@hotmail.com>
2026-01-18 14:00:10 -0300

    We C now in this joint

---
+++ jpeg.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <jpeglib.h>
+#include <jerror.h>
+
+// sudo apt install libjpeg-dev
+// https://github.com/LuaDist/libjpeg/blob/master/example.c
+// https://stackoverflow.com/questions/5616216/need-help-in-reading-jpeg-file-using-libjpeg
+
+typedef struct {
+    unsigned char *data;
+    int width;
+    int height;
+    int channels;
+} Image;
+
+/* Function to read a JPEG file and return an Image struct */
+Image* read_jpeg(const char *filename) {
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    FILE *infile;
+    JSAMPROW row_pointer[1];
+    Image *img = NULL;
+
+    if ((infile = fopen(filename, "rb")) == NULL) {
+        fprintf(stderr, "Can't open %s\n", filename);
+        return NULL;
+    }
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_decompress(&cinfo);
+    jpeg_stdio_src(&cinfo, infile);
+    jpeg_read_header(&cinfo, TRUE);
+
+    /* Set parameters for decompression */
+    cinfo.out_color_space = JCS_RGB; /* Request RGB output color space */
+    cinfo.dct_method = JDCT_ISLOW; /* Use the slow but accurate method */
+
+    jpeg_start_decompress(&cinfo);
+
+    /* Allocate memory for the image data structure and pixel buffer */
+    img = (Image*)malloc(sizeof(Image));
+    if (img == NULL) {
+        fprintf(stderr, "Memory error for Image struct!\n");
+        jpeg_finish_decompress(&cinfo);
+        fclose(infile);
+        return NULL;
+    }
+
+    img->width = cinfo.output_width;
+    img->height = cinfo.output_height;
+    img->channels = cinfo.output_components;
+    long raw_image_size = img->width * img->height * img->channels;
+    img->data = (unsigned char*)malloc(raw_image_size);
+
+    if (img->data == NULL) {
+        fprintf(stderr, "Memory error for image data!\n");
+        free(img);
+        jpeg_finish_decompress(&cinfo);
+        fclose(infile);
+        return NULL;
+    }
+
+    /* Read scanlines */
+    while (cinfo.output_scanline < cinfo.output_height) {
+        row_pointer[0] = &(img->data[cinfo.output_scanline * img->width * img->channels]);
+        jpeg_read_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    jpeg_finish_decompress(&cinfo);
+    fclose(infile);
+    jpeg_destroy_decompress(&cinfo);
+
+    return img;
+}
+
+/* Main function to demonstrate usage */
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <jpeg_file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    Image *image = read_jpeg(argv[1]);
+
+    if (image != NULL) {
+        printf("Successfully read image: %s\n", argv[1]);
+        printf("Dimensions: %d x %d pixels\n", image->width, image->height);
+        printf("Channels: %d\n", image->channels);
+
+        /* Example of accessing a pixel at (x, y) */
+        int x = 10, y = 20;
+        unsigned char *pixel = &(image->data[(y * image->width + x) * image->channels]);
+        printf("Pixel at (%d, %d): R=%u, G=%u, B=%u\n", x, y, pixel[0], pixel[1], pixel[2]);
+
+        /* Free the allocated memory */
+        free(image->data);
+        free(image);
+    } else {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ png.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <png.h>
+
+// http://www.libpng.org/pub/png/book/chapter13.html
+// https://gist.github.com/abforce/2a4dbdeb47d4e6bcaf79de38380a13b9
+// https://stackoverflow.com/questions/65589714/read-and-write-a-png-file-using-libpng-in-c
+
+void read_png_file(const char* filename) {
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) {
+        printf("Error: Could not open file %s\n", filename);
+        return;
+    }
+
+    // 1. Create and initialize the png_structs
+    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png) abort();
+
+    png_infop info = png_create_info_struct(png);
+    if (!info) abort();
+
+    // 2. Set up error handling
+    if (setjmp(png_jmpbuf(png))) abort(); // Required for libpng error handling
+
+    // 3. Initialize IO
+    png_init_io(png, fp);
+
+    // 4. Read file information
+    png_read_info(png, info);
+
+    png_uint_32 width      = png_get_image_width(png, info);
+    png_uint_32 height     = png_get_image_height(png, info);
+    png_byte color_type   = png_get_color_type(png, info);
+    png_byte bit_depth    = png_get_bit_depth(png, info);
+
+    // 5. Set up transformations for a consistent format (e.g., 8-bit RGBA)
+    // Read any color_type into 8bit depth, RGBA format.
+    if (bit_depth == 16)
+        png_set_strip_16(png);
+
+    if (color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_palette_to_rgb(png);
+
+    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand_gray_1_2_4_to_8(png);
+
+    if (png_get_valid(png, info, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(png);
+    
+    if (color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+    if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+        png_set_gray_to_rgb(png);
+
+    png_read_update_info(png, info); // Update info based on transforms
+
+    // 6. Allocate memory for image data (row pointers)
+    png_bytep *row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
+    for (int y = 0; y < height; y++) {
+        row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png, info));
+    }
+
+    // 7. Read the image data
+    png_read_image(png, row_pointers);
+
+    // 8. Process the pixel data (example: print first pixel)
+    png_byte* row = row_pointers[0];
+    png_byte* ptr = &(row[0 * 4]); // Assuming 4 channels (RGBA) after transforms
+    printf("Pixel at [0, 0] has RGBA values: %d - %d - %d - %d\n", ptr[0], ptr[1], ptr[2], ptr[3]);
+
+    // 9. Clean up
+    fclose(fp);
+    for (int y = 0; y < height; y++) {
+        free(row_pointers[y]);
+    }
+    free(row_pointers);
+    png_destroy_read_struct(&png, &info, NULL);
+}
+
+int main() {
+    // Compile with: gcc -std=c99 main.c -lpng
+    read_png_file("test.png");
+    return 0;
+}
